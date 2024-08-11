12534a8708c42b84787f83702f11e44a79d8a803
Codekarish <codeexperts19@gmail.com>
2024-08-11 23:00:32 +0300

    adjusted the file paths

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def search():
         return jsonify({"error": "No matching results found."})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
